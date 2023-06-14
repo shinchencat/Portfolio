bdf8d2f99e986f6dd397990cd884f50a7d366c4c
shinchencat <76943766+shinchencat@users.noreply.github.com>
2023-06-14 09:41:41 +0800

    s

---
+++ 202306_summer/js/common_0323.js
@@ -0,0 +1,213 @@
+// web側選單控制
+function webMenu() {
+	let webMenuBtn = document.querySelectorAll('.arrow_box')
+	webMenuBtn.forEach((item) => {
+		item.onclick = function() {
+			this.parentNode.classList.toggle('go--act');
+		}
+	});
+}
+
+// app上選單
+function appTopMenu() {
+	let appNavBtn = document.querySelector('.nav_btn')
+	let appTopListOpen = document.querySelector('.right_menu')
+	appNavBtn.onclick = function() {
+		this.classList.toggle('go--act');
+		appTopListOpen.classList.toggle('go--act');
+	};
+}
+
+// app下選單控制
+function appMenu() {
+	// 分會場
+	let appMenuBtn = document.querySelector('.more_btn')
+	let subList = document.querySelector('.subBox_app')
+	appMenuBtn.onclick = function() {
+		// 選單滑上
+		subList.classList.toggle('go--act');
+		mask();
+	};
+
+	// app 追蹤我們
+	let appFollowBtn = document.querySelector('.btn_follow')
+	let followBox = document.querySelector('.followBox_app')
+	appFollowBtn.onclick = function() {
+		// 選單滑上
+		followBox.classList.toggle('go--act');
+		mask()
+	}
+
+	// 遮罩&關閉
+	function mask() {
+		// 取得外層容器
+		let box = document.querySelector('.app_menu');
+		// 在容器內創建遮罩
+		let newDiv = document.createElement('div');
+		newDiv.id = "newMask";
+		newDiv.className = "btn_sub";
+		box.appendChild(newDiv);
+
+		// 關閉
+		document.querySelector('#newMask').onclick = function() {
+			// 刪除創建的遮罩
+			this.parentElement.removeChild(this);
+			// 選單滑下
+			followBox.classList.remove('go--act');
+			subList.classList.remove('go--act');
+		}
+	}
+}
+
+//各種輪播
+function allSlider() {
+	document.querySelector('.loading_mask').classList.add('fadeOut');
+	// 版頭輪撥 在filterData調整
+	// 第二個以上的輪播，如有新增輪播，請加在此區往下加
+	var brandtab = new Swiper("#brandtab", {
+		spaceBetween: 0,
+		slidesPerView: 4,
+		freeMode: true,
+		watchSlidesProgress: true,
+	});
+	var swiper2 = new Swiper('#brendslide .swiper-container', {
+		loop: true,
+		mousewheel: false,
+		observer: true, //修改swiper自己或子元素的時候，自動初始化swiper
+		observeParents: true, //修改swiper的父元素時，自動初始化swiper
+		speed: 800,
+		autoplay: {
+			delay: 5000,
+			disableOnInteraction: false,
+		},
+		pagination: {
+			el: '#brendslide .swiper-pagination',
+		},
+		navigation: {
+			nextEl: '#brendslide .swiper-button-next',
+			prevEl: '#brendslide .swiper-button-prev',
+		},
+		thumbs: {
+			swiper: brandtab,
+		},
+	});
+
+
+}
+
+document.addEventListener('DOMContentLoaded', function(event) { 
+	// web側選單控制
+	webMenu();
+	// app上選單控制
+	appTopMenu();
+	// app下選單控制
+	appMenu();
+
+	setTimeout(function () {
+		AOS.init();
+		// 各種輪播
+		allSlider();
+		
+		// hashtag Smooth scrolling
+		// 搭配css (scroll-padding-top)
+		document.querySelectorAll('.scrollto[href^="#"]').forEach(anchor => {
+			anchor.addEventListener('click', function (e) {
+				e.preventDefault();
+				document.querySelector(this.getAttribute('href')).scrollIntoView({
+					behavior: 'smooth',
+					block: "start",
+				});
+			});
+		});
+
+		// 如果a連結的href為#就移除target
+		let getLink_b = document.querySelectorAll('a[href^="#"]'); 
+		getLink_b.forEach(function (item) {
+			item.removeAttribute('target');
+		});
+
+		// 如果a連結的href為空值(#no)就移除href
+		let getLink_a = document.querySelectorAll('a'); 
+		getLink_a.forEach(function (item) {
+			if (item.getAttribute('href') == '#no') {
+				// console.log('移除href');
+				item.removeAttribute('target');
+				item.removeAttribute('href')
+			}
+		});
+	}, 1500);
+	
+	// 網址有hashtag錨點
+	let hashTag = location.hash;
+	if (hashTag != '') {
+		console.log(hashTag);
+		setTimeout(() => {
+			const myElement = document.querySelector(hashTag);
+			myElement.scrollIntoView();
+		}, 1300);
+	} 
+});
+
+
+//新增的動畫
+
+
+// gsap.from(".kv-bigbg", {
+//     scrollTrigger: ".kv-bigbg", // 此行代码表示触发动画的元素，只需要增加该行代码，就可以实现想要的效果。
+//     duration:3,
+//     scale: 2
+// });
+
+
+document.addEventListener("DOMContentLoaded", function() {
+
+
+//.play-smile
+	gsap.from(".play-smile", {
+scrollTrigger: {
+  trigger: ".play-smile",
+  start: "top 90%", // 修改为top 70%
+  end: "bottom 30%",
+  scrub: true,
+},
+opacity: 0,
+duration: 1,
+y: -100, // 修改为Y方向向上位移100
+rotation: 720 // 修改为旋转两圈（720度）
+});
+//.play-smile2
+gsap.from(".play-smile2", {
+scrollTrigger: {
+  trigger: ".play-smile2",
+  start: "top 80%", // 修改为top 70%
+  end: "bottom 30%",
+  scrub: true,
+},
+opacity: 0,
+duration: 1,
+y: -100, // 修改为Y方向向上位移100
+rotation: 720 // 修改为旋转两圈（720度）
+});
+//.cont-bar-right
+gsap.from(".cont-bar-right", {
+	scrollTrigger: {
+	  trigger: ".cont-bar-right",
+	  start: "top 100%", // 修改为top 70%
+	  end: "bottom 30%",
+	  scrub: true,
+	},
+	duration: 3,
+	yPercent: 80
+	});
+//.cont-bar-left
+gsap.from(".cont-bar-left", {
+	scrollTrigger: {
+	  trigger: ".cont-bar-left",
+	  start: "top 100%", // 修改为top 70%
+	  end: "bottom 30%",
+	  scrub: true,
+	},
+	duration: 3,
+	yPercent: 80
+	});
+});
---
+++ 202306_summer/js/filterData_0508.js
@@ -0,0 +1,293 @@
+const timeInMs = Date.now();
+const dataNum = 'C583002590';
+const dataURL = 'https://shopping.pchome.com.tw/cdn/activity/campaign/' + dataNum + '/data?v' + timeInMs;
+const dataScript = document.createElement('script');
+dataScript.type = 'text/javascript';
+dataScript.src = dataURL;
+document.getElementsByTagName('head')[0].appendChild(dataScript);
+
+//讀取後台資料並重組資料格式
+let dataArr = {}
+function toDataArr (arr){
+	arr.forEach(function(item){
+		dataArr[item.BlockId] = item
+	})
+}
+
+//計算區塊實際資料 (因IndexBlock資料中有空白陣列)
+function dataNode(data, blockNumFirst, blockNumLast) {
+	for(let i=blockNumFirst; i<=blockNumLast; i++){
+		if(dataArr[i]){
+			dataArr[i].Nodes.forEach(function(node){
+				if(node.Img.Src){
+					data.push(node)
+				}
+			})
+		}
+	}
+}
+
+// 計算選單資料
+function dataObjNodeMenu(data, blockNumFirst, blockNumLast) {
+	for(let i=blockNumFirst; i<=blockNumLast; i++){
+		if(dataArr[i]){
+			dataArr[i].Nodes.forEach(function(objNode){
+				if(objNode.Link.Url){
+					data.push(objNode)
+				}
+			})
+		}
+	}
+}
+
+//插入選單DOM 
+function insertMenu(webTargetDom, appTargetDom, goMainIcon) {
+	//for Web
+	let newArr = [];
+	let newArr2 = [];
+	let webHtmlImg = '';
+	let webHtmlList = '';
+	let webHtmlGoMain = '';
+	//for APP
+	let appHtmlList = '';
+	let appHtmlGoMain = '';
+	// data
+	dataObjNodeMenu(newArr, 143, 144)
+	dataObjNodeMenu(newArr2, 357, 360)
+	newArrAll = Object.assign([], newArr, newArr2);
+	// console.log(newArrAll);
+	// 無選單隱藏
+	(newArrAll.length > 0) ? ( inHtml() ) : ( document.querySelector('.left_menu').classList.add('hide') );
+	function inHtml() {
+		// 主會場小圖
+		webHtmlImg = ` 
+					<li class="banner head">
+						<a href="`+ newArrAll[0].Link.Url +`">
+							<img src="`+ newArrAll[0].Img.Src +`" class="d-block">
+						</a>
+					</li>
+					<li class="subtitle"><dt>特色分會場</dt></li>`
+	
+		// 分會場
+		for (let i = 2; i < newArrAll.length; i++) {
+			if (newArrAll[i].Img.Src == "") {
+				// (web)分會場list
+				webHtmlList +=`
+					<li>
+						<a href="`+ newArrAll[i].Link.Url +`">`+ newArrAll[i].Link.Text +`</a>
+					</li> `
+			} else {
+				// (web)推薦分會場list
+				webHtmlList +=` 
+					<li class="act_evt">
+						<a href="`+ newArrAll[i].Link.Url +`">
+							<img src="`+ newArrAll[i].Img.Src +`" class="d-block">
+						</a>
+					</li> `
+			}
+			// (app)分會場all list
+			appHtmlList +=`<li class="col-4"><a href="`+ newArrAll[i].Link.Url +`">`+ newArrAll[i].Link.Text +`</a</li> `
+		}
+	
+		// (web)返回主會場
+		webHtmlGoMain =`<li class="act_home"><a href="`+ newArrAll[0].Link.Url +`">返回主會場</a></li>`
+		// (web)輸出html
+		document.querySelector(webTargetDom).innerHTML = webHtmlImg + webHtmlList + webHtmlGoMain
+		// (app)返回主會場
+		appHtmlGoMain =`<a href="`+ newArrAll[0].Link.Url +`"><span class="txt"><span class="ico ico-star"></span>主會場</span></a>`
+		// (app)輸出html
+		document.querySelector(appTargetDom).innerHTML = appHtmlList
+		document.querySelector(goMainIcon).innerHTML = appHtmlGoMain
+	}
+}
+
+// 側選單輪播
+function insertSmallBn(blockNumFirst1, blockNumLast1, targetDom){
+    let html = '';
+    let newArr = [];
+	dataNode(newArr, blockNumFirst1, blockNumLast1);
+	// console.log(newArr.length);
+	(newArr.length > 0) && (inHtml());
+	function inHtml() {
+		newArr.forEach(function (item) {
+			html += `
+					<li class="swiper-slide">
+						<a href="`+ item.Link.Url +`" target="_blank">
+							<div class="pic"><img src="`+ item.Img.Src +`" class="d-block"></div>
+						</a>
+					</li> `
+		});
+		document.querySelector(targetDom).innerHTML = html;
+	}
+
+	// 輪播控制, 2卡以上autoplay
+	setTimeout(() => {
+		let domName = '#smallBn';
+		let ifOneList = document.querySelectorAll(domName +' li').length>1;
+		let smallbn = new Swiper(domName, {
+			autoplay: ifOneList,
+			allowTouchMove: ifOneList,
+			speed: 600,
+			loop: true,
+			effect : 'fade',
+			fadeEffect: {
+				crossFade: true,
+			},
+		});
+	}, 800);
+}
+
+//插入版身商品DOM
+function insertPD(blockNumFirst1, blockNumLast1, targetDom){
+    let html = '';
+    let newArr = [];
+	dataNode(newArr, blockNumFirst1, blockNumLast1);
+	// console.log(newArr.length);
+	(newArr.length > 0) && (inHtml());
+	function inHtml() {
+		newArr.forEach(function (item) {
+			// 原價是否有內容 ture:false
+			let LinkText3 = "";
+			(item.Link.Text3 == "") ? 
+				LinkText3 = "" : LinkText3 = '$'+item.Link.Text3;
+				html += `
+					<li class="hoversacle">
+						<a href="`+ item.Link.Url +`" target="_blank">
+						<div class="box-img"><img
+						src="`+ item.Img.Src +`" />
+				</div>
+				<h6>`+ item.Link.Text2 +`</h6>
+				<h5>`+ item.Link.Text +`</h5>
+				<p class="price"><s>`+ LinkText3 +`</s><span>$<strong>`+item.Link.Text1 +`</strong></span></p>
+						</a>
+					</li> `
+					
+		});
+	}
+    document.querySelector(targetDom).innerHTML = html;
+}
+
+// 插入版身banner DOM (圖+連結)
+ function insertbodyBN(blockNumFirst1, blockNumLast1, targetDom){
+     let html = '';
+     let newArr = [];
+ 	dataNode(newArr, blockNumFirst1, blockNumLast1);
+ 	// console.log(newArr.length);
+ 	(newArr.length > 0) && (inHtml());
+ 	function inHtml() {
+ 		newArr.forEach(function (item) {
+ 			html += `
+ 			<div class="col-6 col-lg-3 hoversacle p-1 px-lg-2"><a href="`+ item.Link.Url +`" target="_blank"><img src="`+ item.Img.Src +`"></a></div>`
+ 		});
+ 	}
+     document.querySelector(targetDom).innerHTML = html;
+ }
+
+//插入強打輪播 DOM
+function insertMainSlider(blockNumFirst1, blockNumLast1, targetDom){
+    let html = '';
+    let newArr = [];
+	dataNode(newArr, blockNumFirst1, blockNumLast1);
+	(newArr.length > 0) && (inHtml());
+	function inHtml() {
+		newArr.forEach(function (item, index) {
+			html += `<li class="swiper-slide">
+					<a href="`+ item.Link.Url +`">
+					<h6>`+ item.Link.Text2 +`</h6>
+					<div class="box-img"><img src="`+ item.Img.Src +`"></div>
+						<h5>`+ item.Link.Text +`</h5>
+						<p class="price">$<strong>`+item.Link.Text1 +`</strong></p>
+					</a>
+				</li>`
+		});
+	}
+    document.querySelector(targetDom).innerHTML = html;
+
+	// 確認載入輪播圖，才會啟動輪播功能，避免版頭壞掉or卡住 
+	// 請注意輪播的 class ID是否相符(#manSlider)
+	const images = document.querySelectorAll(targetDom+' img');
+	console.log(images);
+	(images.length > 0) && (intro());
+	function intro() {
+		setTimeout(() => {
+			var mainSwiperWeb = new Swiper('#mainSlider .swiper-container', {
+				slidesPerView:'auto',
+				spaceBetween: 0,
+				centeredSlides: true,
+				loop: true,
+				mousewheel: false,
+				observer: true, //修改swiper自己或子元素的時候，自動初始化swiper
+				observeParents: true, //修改swiper的父元素時，自動初始化swiper
+				speed: 800,
+				  autoplay: {
+			     delay:4000,
+				disableOnInteraction: false,
+				},
+				breakpoints: {
+					320: {
+						slidesPerView: 1.6,
+					},
+				
+					768: {
+						slidesPerView: 3,
+					},
+					991: {
+						slidesPerView: 4,
+					},
+					1199: {
+						slidesPerView: 'auto',
+					},
+				},
+			});
+		}, 500);
+	}
+}
+//-------------新增一個含tab的品牌輪播-------------//
+
+function insertBrandSlider(blockNumFirst1, blockNumLast1, targetDom){
+    let html = '';
+    let newArr = [];
+	dataNode(newArr, blockNumFirst1, blockNumLast1);
+	(newArr.length > 0) && (inHtml());
+	function inHtml() {
+		newArr.forEach(function (item, index) {
+			html += `<li>
+					<a href="`+ item.Link.Url +`">
+					<div class="box-img"><img src="`+ item.Img.Src +`"></div>
+					<h6>`+ item.Link.Text2 +`</h6>
+				
+						<h5>`+ item.Link.Text +`</h5>
+						<p class="price">$<strong>`+item.Link.Text1 +`</strong></p>
+					</a>
+				</li>`
+		});
+	}
+    document.querySelector(targetDom).innerHTML = html;
+
+}
+//
+
+window.onload = function() {
+	// 接資料
+	toDataArr(IndexBlock);
+	// 主分會場選單
+	insertMenu('.left_menu .menu_box', '#appSubList', '#appMainIcon')
+	// 右側選單小貼紙
+	//  insertSmallBn(29,52, '#smallBn>ul')
+
+	// 強打輪播
+	insertMainSlider(42,51, '#mainSlider ul');
+	// 第二個輪播
+	//insertMainSlider(33,42, '#swiper-new2 ul');
+	// 品牌輪播
+	insertBrandSlider(64,73, '#sb_01');
+	insertBrandSlider(220,229, '#sb_02');
+	insertBrandSlider(231,240, '#sb_03');
+	insertBrandSlider(242,251, '#sb_04');
+	// banner
+	insertbodyBN(31,40, '#pd_bn');
+	// 商品
+	insertPD(53,62, '#pd_kol');
+	insertPD(88,97, '#pd_02');
+	insertPD(99,108, '#pd_01');
+};
